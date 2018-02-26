96a674966b52a0cbd968cd078e9f2dfda5deb1e1
Phillip Truong <philliptruong1@hotmail.com>
2018-02-26 23:02:41 +1100

    center div into middle of page

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Hiragana Quiz</title>
     <!-- FONT -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
     <!-- BOOTSTRAP CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- CSS -->
@@ -12,11 +13,11 @@
   </head>
   <body>
     <div class="container-fluid h-100">
-      <div class="row align-items-center justify-content-center">
-        <div class="col">
-          <div class="mx-auto">
-            <h2 class="text-uppercase">Hiragana Quiz</h2>
-            <button type="button" class="btn btn-primary">Start</button>
+      <div class="row h-100">
+        <div class="col-sm-12 align-self-center">
+          <div class="w-25 mx-auto">
+            <h3 class="text-uppercase font-weight-bold text-center">Hiragana Quiz</h3>
+            <button type="button" class="btn btn-primary w-100">Start</button></div>
           </div>
         </div>
       </div>
