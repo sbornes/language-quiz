e336677393136109f234e549f5d07b91eb8bdff2
Phillip Truong <philliptruong1@hotmail.com>
2018-02-27 10:15:44 +1100

    added quiz index/counter

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 var hiraganaArr = [];
+var count = 0;
+var ArrSize = 0;
+var ArrIndex = 0;
 
 function getHiraganaJson() {
   return $.getJSON( "json/hiragana.json", function( data ) {
     $.each( data, function( key, val ) {
-      hiraganaArr[key] = val;
+      //hiraganaArr[key] = val;
+      hiraganaArr.push({"question": key, "answer": val});
     });
-    console.log("1.0 " + hiraganaArr);
+    $.each(hiraganaArr, function(index, val) {
+        console.log(index + ". " + val.question + " = " + val.answer);
+    });
+    ArrSize = hiraganaArr.length;
   });
 }
 
@@ -13,32 +20,36 @@ function getHiraganaJson() {
 function startQuizClick()
 {
   $.when(getHiraganaJson()).then(newHiragana);
-
 }
 
 function newHiragana()
 {
-    console.log("2.0 " + hiraganaArr);
-    var random_hiragana = fetch_random(hiraganaArr);
-
-    $("#data").load('quiz_card.php', {'hiragana': random_hiragana, 'answer': 'a'});
+  var random_hiragana = random_item(hiraganaArr);
+  count++;
+  console.log("selected hiragana: " + random_hiragana.question + " = " + random_hiragana.answer);
+  $("#data").load('quiz_card.php', {'hiragana': random_hiragana.question, 'answer': random_hiragana.answer, 'qCount' : count, 'qTotal' : ArrSize}, function() {
+    $('#hiragana-quiz-answer').focus();
+  });
 }
 
-function fetch_random(obj) {
-    var temp_key, keys = [];
-    for(temp_key in obj) {
-       if(obj.hasOwnProperty(temp_key)) {
-           keys.push(temp_key);
-       }
-    }
-    return obj[keys[Math.floor(Math.random() * keys.length)]];
+function random_item(items)
+{
+  ArrIndex = Math.floor(Math.random()*items.length);
+  return items[ArrIndex];
 }
 
 // TEXT BOX ANSWER
 $(document).on('keyup', '#hiragana-quiz-answer', function(event) {
-    if (event.keyCode == 13) {
-        alert("Enter pressed");
-        newHiragana();
-        return false; // prevent the button click from happening
+  if (event.keyCode == 13) {
+    if(count == ArrSize) {
+      // Finished Quiz
+      alert("Quiz Finished!");
     }
+    else {
+      console.log("arrIndex: " + ArrIndex);
+      hiraganaArr.splice(ArrIndex, 1);
+      newHiragana();
+    }
+    return false; // prevent the button click from happening
+  }
 });
--- quiz_card.php
+++ quiz_card.php
@@ -1,11 +1,14 @@
 <?php
   $hiragana = !empty($_REQUEST['hiragana']) ? $_REQUEST['hiragana'] : null;
   $answer = !empty($_REQUEST['answer']) ? $_REQUEST['answer'] : null;
+
+  $qCount = !empty($_REQUEST['qCount']) ? $_REQUEST['qCount'] : 0;
+  $qTotal = !empty($_REQUEST['qTotal']) ? $_REQUEST['qTotal'] : 0;
 ?>
 
 <?php if($hiragana != null && $answer != null) : ?>
 <div class="card text-white bg-primary mb-3 mx-auto" style="max-width: 18rem;">
-  <div class="card-header text-uppercase">Hiragana Quiz<span class="float-sm-right">1/46</span></div>
+  <div class="card-header text-uppercase">Hiragana Quiz<span class="quizCounter float-sm-right"><?php echo $qCount . '/' . $qTotal; ?></span></div>
   <div class="card-body">
     <h5 class="card-text hiragana-text text-center"><?php echo $hiragana; ?></h5>
   </div>
