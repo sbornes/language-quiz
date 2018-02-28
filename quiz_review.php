dd55b5ac56fcdd828ec31c2a8cebf188e03a5bbf
Phillip Truong <philliptruong1@hotmail.com>
2018-03-01 10:51:18 +1100

    made quiz review table headers sticky

--- quiz_review.php
+++ quiz_review.php
@@ -2,23 +2,25 @@
   $hiraganaArr = !empty($_REQUEST['hiragana-review']) ? $_REQUEST['hiragana-review'] : null;
 ?>
 
-<table class="table table-sm-responsive text-center">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Hiragana</th>
-      <th scope="col">Answer</th>
-      <th scope="col">Your Answer</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php for ($i=0; $i < sizeof($hiraganaArr); $i++) : ?>
-      <tr class="<?php echo $hiraganaArr[$i]['answer'] == $hiraganaArr[$i]['your_answer'] ? "bg-success" : "bg-danger"; ?>">
-        <th scope="row"><?php echo $i+1; ?>.</th>
-        <td><?php echo $hiraganaArr[$i]['question']; ?></td>
-        <td><?php echo $hiraganaArr[$i]['answer']; ?></td>
-        <td><?php echo $hiraganaArr[$i]['your_answer']; ?></td>
+<div class="table-sm-responsive">
+  <table class="table text-center">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col" class="sticky-top">#</th>
+        <th scope="col" class="sticky-top">Hiragana</th>
+        <th scope="col" class="sticky-top">Answer</th>
+        <th scope="col" class="sticky-top">Your Answer</th>
       </tr>
-    <?php endfor; ?>
-  </tbody>
-</table>
+    </thead>
+    <tbody>
+      <?php for ($i=0; $i < sizeof($hiraganaArr); $i++) : ?>
+        <tr class="<?php echo $hiraganaArr[$i]['answer'] == $hiraganaArr[$i]['your_answer'] ? "bg-success" : "bg-danger"; ?>">
+          <th scope="row"><?php echo $i+1; ?>.</th>
+          <td><?php echo $hiraganaArr[$i]['question']; ?></td>
+          <td><?php echo $hiraganaArr[$i]['answer']; ?></td>
+          <td><?php echo $hiraganaArr[$i]['your_answer']; ?></td>
+        </tr>
+      <?php endfor; ?>
+    </tbody>
+  </table>
+</div>
