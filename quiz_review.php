5cdfca101b62e67629b76190968c2bf02bb409ed
Phillip Truong <philliptruong1@hotmail.com>
2018-03-08 22:24:36 +1100

    fixed names

--- quiz_review.php
+++ quiz_review.php
@@ -1,5 +1,5 @@
 <?php
-  $hiraganaArr = !empty($_REQUEST['quiz-review']) ? $_REQUEST['quiz-review'] : null;
+  $quiz_review = !empty($_REQUEST['quiz-review']) ? $_REQUEST['quiz-review'] : null;
 ?>
 
 <div class="table-responsive text-center h-100">
@@ -13,12 +13,12 @@
       </tr>
     </thead>
     <tbody>
-      <?php for ($i=0; $i < sizeof($hiraganaArr); $i++) : ?>
-        <tr class="<?php echo strtoupper($hiraganaArr[$i]['answer']) == strtoupper($hiraganaArr[$i]['your_answer']) ? "bg-success" : "bg-danger"; ?>">
+      <?php for ($i=0; $i < sizeof($quiz_review); $i++) : ?>
+        <tr class="<?php echo strtoupper($quiz_review[$i]['answer']) == strtoupper($quiz_review[$i]['your_answer']) ? "bg-success" : "bg-danger"; ?>">
           <th scope="row"><?php echo $i+1; ?>.</th>
-          <td><?php echo $hiraganaArr[$i]['question']; ?></td>
-          <td><span class="text-lowercase"><?php echo $hiraganaArr[$i]['answer']; ?></span></td>
-          <td><span class="text-lowercase"><?php echo $hiraganaArr[$i]['your_answer']; ?></span></td>
+          <td><?php echo $quiz_review[$i]['question']; ?></td>
+          <td><span class="text-lowercase"><?php echo $quiz_review[$i]['answer']; ?></span></td>
+          <td><span class="text-lowercase"><?php echo $quiz_review[$i]['your_answer']; ?></span></td>
         </tr>
       <?php endfor; ?>
     </tbody>
